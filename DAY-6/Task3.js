1b6cccd3559c26c1c0f37d8a6273b643f07dcdac
VISHALKRBIBHUTI <vishalkrbibhuti@gmail.com>
2024-07-19 17:55:24 +0530

    clear

--- DAY-6/Task3.js
+++ DAY-6/Task3.js
@@ -1,4 +1,15 @@
 //pop
 
-let array=[1,2,3,4,5]
-console.log(array.pop())
+let array=[1,2,3,4,5];
+
+console.log("original Array :");
+
+for(let i=0;i<array.length;i++){
+    console.log(array[i])
+}
+array.pop()
+
+console.log("After Pop ")
+for(let i=0;i<array.length;i++){
+    console.log(array[i])
+}    
--- DAY-6/Task4.js
+++ DAY-6/Task4.js
@@ -1,2 +1,12 @@
-let array=[1,2,3,4,5]
-console.log(array.push(6))
+let array=[1,2,3,4,5];
+console.log("original Array: ");
+
+for(let i=0;i<array.length;i++){
+    console.log(array[i]);
+}
+array.push(6);
+
+console.log("After Push 6 in the array ");
+for(let i=0;i<array.length;i++){
+    console.log(array[i]);
+}
